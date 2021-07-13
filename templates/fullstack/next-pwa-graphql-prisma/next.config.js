c87eb78e087c00fe5c7cc07e5efbec2727f428ab
gnarus-g <g.avet.gnaritas@gmail.com>
2021-07-13 16:52:16 -0400

    updating the settings for next-pwa in the relevant templates

--- templates/fullstack/next-pwa-graphql-prisma/next.config.js
+++ templates/fullstack/next-pwa-graphql-prisma/next.config.js
@@ -1,7 +1,11 @@
 const withPWA = require('next-pwa')
- 
+
 module.exports = withPWA({
     pwa: {
-        dest: 'public'
+        dest: 'public',
+		disable: process.env.NODE_ENV !== 'production'
+    },
+    env: {
+        REST_API_URL: process.env.REST_API_URL
     }
-})
+});
