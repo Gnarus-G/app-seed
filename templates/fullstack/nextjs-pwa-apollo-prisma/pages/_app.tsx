1bd775ab8deb37bfcd08f0b1e73b1a4cdca1d739
gnarus-g <g.avet.gnaritas@gmail.com>
2021-07-13 16:58:24 -0400

    refactor: cleaning up the _app.tsx in nextjs templates

--- templates/fullstack/nextjs-pwa-apollo-prisma/pages/_app.tsx
+++ templates/fullstack/nextjs-pwa-apollo-prisma/pages/_app.tsx
@@ -7,20 +7,19 @@ import theme from '../src/theme';
 export default function MyApp(props: AppProps) {
     const { Component, pageProps } = props;
 
-    React.useEffect(() => {
-        // Remove the server-side injected CSS.
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles) {
-            jssStyles.parentElement!.removeChild(jssStyles);
-        }
-    }, []);
+    React.useEffect(removeServerSideInjectedCSS, []);
 
     return (
-        <>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+        </ThemeProvider>
     );
+}
+
+function removeServerSideInjectedCSS() {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+        jssStyles.parentElement!.removeChild(jssStyles);
+    }
 }
